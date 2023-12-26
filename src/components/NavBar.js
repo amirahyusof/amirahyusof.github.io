3d82b911fc6c60dca6a52702cde127576b6dd283
amirah <mirez.data@gmail.com>
2023-12-27 00:52:16 +0800

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React from "react"
+import styles from "../../src/css/Navbar.module.css"
 
 function NavBar(){
     
     return(
-        <nav className="navbar">
-          <a href="/">Amirah Yusof</a>
-          <div className="navbar--content">
-            <ul>
+        <nav className={styles.navbar}>
+          <a className={styles.title} href="/">AmirahYusof</a>
+          <div className ={styles.menu}>
+            <ul className={styles.menuItems}>
               <li><a href="#about">About</a></li>
               <li><a href="#skills">Skills</a></li>
               <li><a href="#projects">Projects</a></li>
