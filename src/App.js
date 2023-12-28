e93b7071b0e8d69ab6c204397ec06f95af82ddcc
amirah <mirez.data@gmail.com>
2023-12-28 22:37:37 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './index.css';
+import styles from './css/App.module.css'
 import NavBar from './components/NavBar';
 import Hero from './components/Hero'
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
       <Hero />
     </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ function Hero(){
             <div>
             <h1 className={styles.heroHeader}>Hi, I'm Amirah</h1>
             <h2><span className={styles.heroTitle}>I'm front-end developer</span></h2>
-            <p className={styles.heroIntro}>Reach me out if you'd like to want to know more</p>
+            <p className={styles.heroIntro}>Reach me out if you'd want to know more</p>
             <button type='submit'className={styles.resumeButton}>Resume</button>
             </div>
             <div>
