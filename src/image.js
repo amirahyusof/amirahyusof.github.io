c3c246883113c91b8446d2ba503feb7b7464da43
amirah <mirez.data@gmail.com>
2023-12-28 22:42:27 +0800

    commit

--- src/image.js
+++ src/image.js
@@ -1,3 +1,4 @@
-export const getimageUrl =(path) => {
-    return new URL(`assets/$(path)', import.meta.url).href
-}
+export const getImageUrl = (path) => {
+    return new URL(`assets/${path}`, import.meta.url).href;
+
+};
